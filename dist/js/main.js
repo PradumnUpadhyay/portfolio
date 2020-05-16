c901762ac2463042837ab5aed4ac041ef46ae51a
PradumnUpadhyay <pradumn.upadhyay1@gmail.com>
2020-05-17 02:18:45 +0530

    Final Commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,3 +32,16 @@ function toggleMenu() {
         sm=false;
     }
 }
+
+let tdy=new Date();
+let hrs=tdy.getHours();
+var greet;
+
+if (hrs < 12)
+    greet = 'Good Morning :)';
+else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon :)';
+else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening :)';
+
+document.getElementById('greeting').innerHTML='<h1>'+greet+'</h1>';
